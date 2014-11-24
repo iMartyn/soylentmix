b4de59d4e1421e2f2660cb4f3032c5532835586f
Martyn Ranyard <martyn.ranyard@bskyb.com>
2014-11-24 18:29:31 +0000

    initial cut

---
+++ mixer.cpp
@@ -0,0 +1,73 @@
+/* 
+ * File:   mixer.cpp
+ * Author: ranyardm
+ *
+ * Created on 24 November 2014, 17:52
+ */
+
+#include <cstdlib>
+#include <time.h>
+#include "mixer.h"
+
+using namespace std;
+
+#ifndef ARDUINO
+void sleep(int) {}
+#endif
+
+class Mixer {
+    // The pins for the servos on the hoppers
+    int hoppers[MAX_HOPPERS];
+public:
+    Mixer();
+    int takeReading();
+    int takeAverageReading(int,int);
+    void pourPowder(int,int);
+    void openHopper(int);
+    void closeHopper(int);
+};
+
+Mixer::Mixer () {
+    srand (time(NULL));
+    hoppers[0] = 1;
+    hoppers[1] = 2;
+    hoppers[2] = 3;
+    hoppers[3] = 4;
+    hoppers[4] = 5;
+}
+
+void Mixer::pourPowder(int hopper, int weightValue) {
+    int startValue = takeAverageReading(DEFAULT_READINGS, DEFAULT_DELAY);
+    int targetValue = startValue + weightValue;
+    while (takeAverageReading(DEFAULT_READINGS, DEFAULT_DELAY), targetValue) {
+        this->openHopper(hopper); // will have no effect if already open
+    }
+    this->closeHopper(hopper);
+}
+
+int Mixer::takeAverageReading(int averageHowManyReadings, int delayBetweenReadings) {
+    int total = 0;
+    for (int i=0;i<averageHowManyReadings;i++) {
+        total += this->takeReading();
+        if (i<averageHowManyReadings-1) {
+            sleep(delayBetweenReadings);
+        }
+    }
+    return total/averageHowManyReadings;
+}
+
+int Mixer::takeReading() {
+    int val = rand() % 500 + 50;
+    printf("returning %i\n", val);
+    return val;
+}
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    Mixer aMixer;
+    int avg = aMixer.takeAverageReading(4,1);
+    printf("actual average %i", avg);
+    return 0;
+}
+
---
+++ mixer.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   mixer.h
+ * Author: ranyardm
+ *
+ * Created on 24 November 2014, 17:57
+ */
+
+#ifndef MIXER_H
+#define	MIXER_H
+
+#define DEFAULT_READINGS 5 // average over this many
+#define DEFAULT_DELAY 5 // in millis
+#define MAX_HOPPERS 7
+
+#endif	/* MIXER_H */
+
