091b31786a15441c6e177a67cebfa8094b3d4013
Martyn Ranyard <ranyardm@gmail.com>
2014-11-29 23:29:07 +0000

    Added the flattened curve to the reading algo

--- mixer.cpp
+++ mixer.cpp
@@ -22,6 +22,7 @@ public:
     Mixer();
     int takeReading();
     int takeAverageReading(int,int);
+    float readingToGrams(int);
     void pourPowder(int,int);
     void openHopper(int);
     void closeHopper(int);
@@ -29,11 +30,11 @@ public:
 
 Mixer::Mixer () {
     srand (time(NULL));
-    hoppers[0] = 1;
-    hoppers[1] = 2;
-    hoppers[2] = 3;
-    hoppers[3] = 4;
-    hoppers[4] = 5;
+    hoppers[0] = HOPPER_PIN_1;
+    hoppers[1] = HOPPER_PIN_2;
+    hoppers[2] = HOPPER_PIN_3;
+    hoppers[3] = HOPPER_PIN_4;
+    hoppers[4] = HOPPER_PIN_5;
 }
 
 void Mixer::openHopper(int) {
@@ -68,12 +69,22 @@ int Mixer::takeReading() {
     int val = rand() % 500 + 50;
     return val;
 }
-/*
- * 
- */
+
+float Mixer::readingToGrams(int reading) {
+    if (reading < 1) return 0;
+    float lookupIndex [] = READINGS_TO_GRAMS_INDEX;
+    float lookupValue [] = READINGS_TO_GRAMS_VALUES;
+    for (int i=0; i<READINGS_TO_GRAMS_COUNT; i++) {
+        if (reading >= lookupIndex[i]) {
+            return reading*lookupValue[i];
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     Mixer aMixer;
     int avg = aMixer.takeAverageReading(4,1);
+    sprintf("Average: %i  Grams: %f",avg, aMixer.readingToGrams(avg));
     return 0;
 }
 
--- mixer.h
+++ mixer.h
@@ -12,5 +12,19 @@
 #define DEFAULT_DELAY 5 // in millis
 #define MAX_HOPPERS 7
 
+#define BASE_ZERO_READING 817
+
+// Correction "curve" for readings to grams
+#define READINGS_TO_GRAMS_COUNT 10
+#define READINGS_TO_GRAMS_INDEX {817,825,832,851,889,927,969,1007,1577,1957}
+#define READINGS_TO_GRAMS_VALUES {3.8,3.7,3.6,3.5,3.4,3.3,3.2,3.18,3.16,3.13}
+
+#define HOPPER_PIN_1 UNDEFINED
+#define HOPPER_PIN_2 UNDEFINED
+#define HOPPER_PIN_3 UNDEFINED
+#define HOPPER_PIN_4 UNDEFINED
+#define HOPPER_PIN_5 UNDEFINED
+
+
 #endif	/* MIXER_H */
 
